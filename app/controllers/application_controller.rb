74173bc51d29f37f33d2af70d5accbde7b1d89c0
wizaqua <aquazero@outlook.jp>
2020-06-05 02:44:40 +0900

    アクションの追加とルーティング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "hello, Ruby on rails"
+  end
+
+  def goobye
+    render html: "goobye,Ruby on rails"
   end
 end
